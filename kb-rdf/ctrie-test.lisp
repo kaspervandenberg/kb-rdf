d2f7a7bfd377a68559c92db6f335d15bbdb811ac
Kasper van den Berg <kasper@kaspervandenberg.net>
2018-07-17 22:23:35 +0200

    Display ctries

--- kb-rdf/ctrie-test.lisp
+++ kb-rdf/ctrie-test.lisp
@@ -20,3 +20,29 @@
 	      'list)))
     (format nil "~a [shape = record; label =\"~{<~a>~^|~}\"];~%"
 	    (sxhash x) (mapcar #'(lambda (x) (sxhash x)) brs))))
+
+
+
+(require :asdf)
+(asdf:load-system :uiop)
+
+
+(defun display-ctrie (ctrie)
+  (uiop:with-temporary-file (:pathname png-data :keep t)
+    (let ((dot-process (uiop:launch-program `("/usr/bin/dot" "-Tpng" ,(concatenate 'string "-o" (namestring png-data))) :input :stream)))
+      (let ((dot-program (uiop:process-info-input dot-process)))
+	(format dot-program "digraph g {~%")
+	(net.kaspervandenberg.kb-rdf.ctrie:print-dot ctrie dot-program)
+	(format dot-program "}~%")
+	(uiop:close-streams dot-process))
+      (uiop:wait-process dot-process)
+      (uiop:launch-program `("/usr/bin/display" ,(namestring png-data))))))
+
+
+(let ((x (net.kaspervandenberg.kb-rdf.ctrie:add-intern
+	  (make-instance 'net.kaspervandenberg.kb-rdf.ctrie:SNode :key 'one :value 1)
+	  'two
+	  (sxhash 'two)
+	  1
+	  2)))
+  (display-ctrie x))
